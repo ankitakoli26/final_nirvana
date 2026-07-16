16508d64a94ab637a33ee81b8ca3ad5e21e50018
ankitakoli26 <koliankita3026@gmail.com>
2026-07-16 14:57:07 +0530

    Add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:   '#2E8B57',
+        teal:      '#3AAFA9',
+        purple:    '#7C3AED',
+        lavender:  '#D9C7FF',
+        mint:      '#CFF7E6',
+        navy:      '#0a1628',
+        'navy-mid':'#1a2a4a',
+      },
+      fontFamily: {
+        sans:    ['DM Sans', 'sans-serif'],
+        display: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
